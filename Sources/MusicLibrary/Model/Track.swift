61c3e3dadd29832562b4a0147ddd50726dccfc13
fwcd <fwcdmail@gmail.com>
2023-04-07 21:48:34 +0200

    Document Track fields

--- Sources/MusicLibrary/Model/Track.swift
+++ Sources/MusicLibrary/Model/Track.swift
@@ -42,41 +42,77 @@ public struct Track: Codable, Hashable, Identifiable {
         case libraryFolderCount = "Library Folder Count"
     }
 
+    /// The ID, corresponding to the key in `Library.tracks`.
     public var id: Int
+    /// The title of the track.
     public var title: String?
+    /// Information about the artist that iTunes associates with the track.
     public var artist: String?
+    /// The artist of the track's album.
     public var albumArtist: String?
+    /// The title of the track's album.
     public var album: String?
+    /// The name of the composer that iTunes associates with the track.
     public var composer: String?
+    /// The genre of the track, if any.
     public var genre: String?
+    /// The kind of track file, such as an MPEG audio file.
     public var kind: String?
+    /// The kind of track.
     public var mediaKind: MediaKind?
+    /// The size in bytes of this track on disk.
     public var fileSize: UInt64?
+    /// The length of the track in milliseconds.
     public var totalTime: Int?
+    /// The disc number in the track’s album.
     public var discNumber: Int?
+    /// The number of discs in the track’s album.
     public var discCount: Int?
+    /// The position of the track within its album.
     public var trackNumber: Int?
+    /// The track count of the track’s album.
     public var trackCount: Int?
+    /// The release year of the track.
     public var year: Int?
+    /// The beats-per-minute (BPM) of the track.
     public var bpm: Int?
+    /// The date and time that iTunes last modified the track.
     public var dateModified: Date?
+    /// The date and time that the user added the track to the iTunes library.
     public var dateAdded: Date?
+    /// The bitrate of the track in kbit/s.
     public var bitRate: Int?
+    /// The sample rate of the track in samples-per-second.
     public var sampleRate: Int?
+    /// Any comments that iTunes associates with the track.
     public var comments: String?
+    /// The number of times the user has played the track.
     public var playCount: Int?
+    /// The date and time the user last played the track, or nil if the user hasn’t played it.
     public var playDate: Int?
+    /// The date and time the user last played the track, or nil if the user hasn’t played it.
     public var playDateUtc: Date?
+    /// The volume normalization energy that iTunes applies to the track to bring the average or peak amplitude to a target level.
     public var volumeNormalizationEnergy: Int?
+    /// The release date of the track.
     public var releaseDate: Date?
+    /// The number of artwork images associated with the track.
     public var artworkCount: Int?
+    /// The title of the album to use when sorting.
     public var sortAlbum: String?
+    /// The name of the artist to use when sorting.
     public var sortArtist: String?
+    /// The title of the track to use when sorting.
     public var sortTitle: String?
+    /// The name to use when sorting by composer.
     public var sortComposer: String?
+    /// The persistent ID of the track.
     public var persistentId: String?
+    /// A Boolean value that indicates whether the user purchased the track from the iTunes Store.
     public var isPurchased: Bool?
+    /// The location of the track on disk.
     public var location: String?
+    /// The type of the track with respect to its location.
     public var locationType: LocationType?
     public var fileFolderCount: Int?
     public var libraryFolderCount: Int?
