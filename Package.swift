5598601d7cbf7cb5423548371866cb9fcb579b90
fwcd <fwcdmail@gmail.com>
2023-04-07 18:30:43 +0200

    Declare linked system frameworks in Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MusicLibrary",
-            dependencies: []
+            dependencies: [],
+            linkerSettings: [
+                .linkedFramework("iTunesLibrary", .when(platforms: [.macOS])),
+                .linkedFramework("MediaPlayer", .when(platforms: [.iOS, .macCatalyst])),
+            ]
         ),
         .testTarget(
             name: "MusicLibraryTests",
