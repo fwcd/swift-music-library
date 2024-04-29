a84c08483892d0a8800ed939f3688e2095164569
fwcd <fwcd@fwcd.dev>
2024-04-29 18:59:23 +0200

    Remove 'mutating' from LibraryExporter

--- Sources/MusicLibrary/Export/LibraryExporter.swift
+++ Sources/MusicLibrary/Export/LibraryExporter.swift
@@ -1,11 +1,11 @@
 /// A facility for writing a library.
 public protocol LibraryExporter {
     /// Writes the library.
-    mutating func write(library: Library, onProgress: (ProgressInfo) -> Void) throws
+    func write(library: Library, onProgress: (ProgressInfo) -> Void) throws
 }
 
 public extension LibraryExporter {
-    mutating func write(library: Library) throws {
+    func write(library: Library) throws {
         try write(library: library, onProgress: { _ in })
     }
 }
