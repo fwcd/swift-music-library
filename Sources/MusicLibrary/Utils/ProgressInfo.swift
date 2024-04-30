ec96a8e65ac7973ae0f7db20a33e254331997992
fwcd <fwcd@fwcd.dev>
2024-04-30 02:16:59 +0200

    Make ProgressInfo initializer public

--- Sources/MusicLibrary/Utils/ProgressInfo.swift
+++ Sources/MusicLibrary/Utils/ProgressInfo.swift
@@ -1,7 +1,17 @@
 public struct ProgressInfo {
-    public var current: Int = -1
-    public var total: Int? = nil
-    public var message: String? = nil
+    public var current: Int
+    public var total: Int?
+    public var message: String?
+
+    public init(
+        current: Int = -1,
+        total: Int? = nil,
+        message: String? = nil
+    ) {
+        self.current = current
+        self.total = total
+        self.message = message
+    }
 
     public mutating func increment(message: String? = nil) {
         current += 1
