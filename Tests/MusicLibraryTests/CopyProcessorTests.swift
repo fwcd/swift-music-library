d510d8cf4248dbc13b472a11db70cfa0bd584639
fwcd <fwcd@fwcd.dev>
2024-06-18 04:29:44 +0200

    Test skip predicates

---
+++ Tests/MusicLibraryTests/CopyProcessorTests.swift
@@ -0,0 +1,26 @@
+import XCTest
+@testable import MusicLibrary
+
+final class CopyProcessorTests: XCTestCase {
+    func testSkipPredicate() {
+        let a = URL(filePath: "/tmp/this-file-probably-does-not-exist-830649")
+        let b = URL(filePath: "/tmp/this-file-probably-does-not-exist-159224")
+
+        XCTAssertEqual(CopyProcessor.SkipPredicate.always.skipReason(oldURL: a, newURL: b), "always")
+
+        XCTAssertEqual(CopyProcessor.SkipPredicate.and([]).skipReason(oldURL: a, newURL: b), "empty and")
+        XCTAssertEqual(CopyProcessor.SkipPredicate.and([.always]).skipReason(oldURL: a, newURL: b), "always")
+        XCTAssertEqual(CopyProcessor.SkipPredicate.and([.always, .always]).skipReason(oldURL: a, newURL: b), "always and always")
+        XCTAssertNil(CopyProcessor.SkipPredicate.and([.never]).skipReason(oldURL: a, newURL: b))
+        XCTAssertNil(CopyProcessor.SkipPredicate.and([.always, .never]).skipReason(oldURL: a, newURL: b))
+        XCTAssertNil(CopyProcessor.SkipPredicate.and([.never, .always]).skipReason(oldURL: a, newURL: b))
+        XCTAssertNil(CopyProcessor.SkipPredicate.and([.never, .never]).skipReason(oldURL: a, newURL: b))
+
+        XCTAssertNil(CopyProcessor.SkipPredicate.or([]).skipReason(oldURL: a, newURL: b))
+        XCTAssertNil(CopyProcessor.SkipPredicate.or([.never]).skipReason(oldURL: a, newURL: b), "always")
+        XCTAssertEqual(CopyProcessor.SkipPredicate.or([.always]).skipReason(oldURL: a, newURL: b), "always")
+        XCTAssertEqual(CopyProcessor.SkipPredicate.or([.always, .always]).skipReason(oldURL: a, newURL: b), "always")
+        XCTAssertEqual(CopyProcessor.SkipPredicate.or([.never, .always]).skipReason(oldURL: a, newURL: b), "always")
+        XCTAssertEqual(CopyProcessor.SkipPredicate.or([.always, .never]).skipReason(oldURL: a, newURL: b), "always")
+    }
+}
